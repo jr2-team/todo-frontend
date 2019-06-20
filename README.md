f2192e26c522f690f2e2eb51db6b758f4004e4ff
PNZeml <pavelz1n@yandex.ru>
2019-06-20 10:06:24 +0700

    Update README Fix npm scripts to run in hot reload mode Add rules to tslint

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # todo-frontend
+
+## Building and running
+Install dependencies from npm:
+~~~
+npm install
+~~~
+
+In order to run with a hot reload need to open two terminals and run the next commands:
+~~~
+:: in the first terminal
+npm run build
+:: in the second
+npm run start
+:: or create a .bat file and run it (Windows only)
+start npm run build && npm run start
+~~~
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import BaseScreen from "./components/BaseScreen";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import BaseScreen from './components/BaseScreen';
 
 ReactDOM.render(
-    
-    <BaseScreen {...{title: "Hello how do you do?"}}></BaseScreen>,
-    document.getElementById("example")
+    <BaseScreen {...{title: '!!!'}}/>,
+    document.getElementById('root'),
 );
